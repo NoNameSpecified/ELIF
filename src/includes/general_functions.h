4579a65964dc757a55f12b8eae4eca8eaf00ca8b
Kendrik <57840931+NoNameSpecified@users.noreply.github.com>
2025-03-09 02:36:35 +0100

    Update and rename src/general_functions.h to src/includes/general_functions.h

--- src/includes/general_functions.h
+++ src/includes/general_functions.h
@@ -3,16 +3,24 @@
 
 #include "define_colours.h" // because we print with colours.
 
+//
 // Small, general functions. We use a lot of ANSI escape sequences.
+//
+
 static void hide_cursor() { printf("\033[?25l"); }
 static void show_cursor() { printf("\033[?25h"); }
-static void clear_screen() { printf("\033[H\033[J"); printf(RESET); } // deletes screen and sets cursor to top line
+// deletes screen and sets cursor to top line but also resets color
+static void clear_screen() { printf("\033[H\033[J"); printf(RESET); }
+// simple delete and reset to default
+static void reset_screen() { printf("\033[H\033[J"); printf(RESET_DEFAULT); }
 // these functions arent always used, sometimes i just set specific colours in the code itself.
 static void print_prompt(char *text){  printf(DARK_BLUE " %s \n\n" RESET, text);  }
 static void print_menu(char *text){  printf(DARK_BLUE "    %s   " RESET, text);  }
-static void print_selected(char *text){  printf(NEW_WHITE "  > %s < " RESET, text);  }
+static void print_highlighted(char *text){  printf(NEW_WHITE "  > %s < " RESET, text);  }
+static void print_selected(char *text){  printf(RED "  *" DARK_BLUE " %s   " RESET, text);  }
+static void print_selected_highlighted(char *text){  printf(RED "  *" NEW_WHITE " %s   " RESET, text);  }
 static void print_magenta(char *text){  printf(MAGENTA " %s \n\n" RESET, text);  }
-
+static void move_cursor_up(const int lines) { printf("\033[%iA", lines); } // moves cursor up X lines.
 static void move_cursor_precise(const int row, const int col) {
     printf("\033[%d;%dH", row, col);  // moves cursor to position
     fflush(stdout); // print immediately.
@@ -41,6 +49,7 @@ static int count_options(char *options[]){
     return count;
 }
 
+// Calculate padding when we want to center according to the largest element.
 static char *calculate_padding(const char *longest_string, const char *string_to_pad){
     // default padding that even the longest string must have.
     const int default_padding = 6; // so 3 spaces left, 3 right.
@@ -52,7 +61,7 @@ static char *calculate_padding(const char *longest_string, const char *string_to
     if (longest_string_len % 2 != 0){ longest_string_len ++; }
     if (string_to_pad_len % 2 != 0){ string_to_pad_len ++; }
 
-    int padding = (longest_string_len - string_to_pad_len) / 2;
+    const int padding = (longest_string_len - string_to_pad_len) / 2;
     char *padding_str = malloc(padding + 1); // + 1 for null-terminator \0.
     // memset to fill with our padding variable with spaces.
     memset(padding_str, ' ', padding);
